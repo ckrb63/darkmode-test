67d2979bd2d84357db50fec9b906e6b18a4ebf71
ckrb63 <cksr1@naver.com>
2022-05-25 21:06:25 +0900

    findword api connect

--- src/pages/FindWord.tsx
+++ src/pages/FindWord.tsx
@@ -1,23 +1,12 @@
 import classNames from "classnames";
-import React, { useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
+import axios from "axios";
 import "./FindWord.scss";
 
-const textArray = [`이 책을 어른에게 바치는데 대하여 어린이들에게 용서를 빈다. 여기에는
-중요한 이유가 있다.그것은 이 어른이 세상에서 나와 가장 친한 친구라는
-것이다.또 다른 한 이유는 이 어른이 나의 모든 점을 이해할 수 있고
-어린이들을 위해 씌어진 책들까지도 이해할 수 있다는 것이다.세 번째
-이유는 이 어른이 프랑스에서 살고 있는데 그곳에서 그는 굶주리고 추위에
-떨고 있다는 것이다.이 어른을 잘 위로해 주어야 한다.만일 이 모든
-이유가 충분하지 않다면 나는 이 책을 이 어른이 옛날 어린이로 있던
-시절에 기꺼이 바치고 싶다.모든 어른들은 어린이였다.그래서 나는
-헌사를 이렇게 고쳐 쓴다.`, `수많은 인코딩들이 제한된 문자열들만을 보유하고 있기 때문에 
-제한된 하위 집합의 인간 언어로 텍스트를 표현할 때에만 종종 유용하다. 
-유니코드는 알려진 모든 언어를 대표하기 위한 공통 표준을 만드는 시도로
- 볼 수 있으며, 알려진 대부분의 문자 집합들이 매우 큰 유니코드 문자 
- 집합의 하위 집합에 속해있다. 유니코드를 위한 복수의 문자 인코딩이 
- 있지만, 가장 흔한 것은 UTF-8이며, ASCII와 하위 호환된다는 장점이 
- 있다. 즉, 비슷한 의미로 모든 ASCII 텍스트 파일은 UTF-8 텍스트 
- 파일이기도 하다.`];
+interface Text{
+  text:string,
+  word:string
+}
 
 const wordArray = ['그림', '미술'];
 
@@ -25,10 +14,21 @@ const FindWord = () => {
   const [mode, setMode] = useState(false);
   const [started, setStarted] = useState(false);
   const [sequence, setSequence] = useState(0);
+  const [textArray, setTextArray] = useState<any>();
+  const [isEnterd, setIsEntered] = useState(false);
   const sunIcon = <img className="icon" src="https://cdn-icons-png.flaticon.com/512/169/169367.png" alt="sun" />
   const moonIcon = <img className="icon" src="https://cdn-icons-png.flaticon.com/512/1281/1281189.png" alt="moon" />
-
   const icon = mode ? moonIcon : sunIcon;
+
+  useEffect(()=>{
+    getText();
+  },[]);
+
+  const getText = async () => {
+    const response = await axios.get(`http://172.30.1.60:8080/finding-word`);
+    console.log(response);
+    setTextArray(response.data.data);
+  };
 
   const submitAnswerHandler = () => {
     setMode(!mode);
@@ -36,21 +36,59 @@ const FindWord = () => {
     setStarted(false);
   };
 
+  const onChangeHandler = (e:ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    if(e.target.value.length)
+      setIsEntered(true);
+  };
+
+  const finalSubmitHandler = async () => {
+    const response = await axios.post(`http://172.30.1.60:8080/finding-word`,{
+      answers : [
+        {
+          findingWordId: 1,
+          answer:7,
+          uiMode:'DARK',
+          estimatedSeconds:30
+        },
+        {
+          findingWordId: 1,
+          answer:7,
+          uiMode:'DARK',
+          estimatedSeconds:30
+        },
+        {
+          findingWordId: 1,
+          answer:7,
+          uiMode:'DARK',
+          estimatedSeconds:30
+        },
+        {
+          findingWordId: 1,
+          answer:7,
+          uiMode:'DARK',
+          estimatedSeconds:30
+        },
+      ]
+    });
+    console.log(response);
+  };  
+
   return <div className={classNames("findword", { dark: mode, light: !mode })}>
 
     <div className="padding-wrapper">
-      <h1 className="findword-question">{`'${wordArray[sequence]}'(이)라는 단어가 총 몇 번 나오나요?`}</h1>
+      {textArray && <h1 className="findword-question">{`'${textArray[sequence].word}'(이)라는 단어가 총 몇 번 나오나요?`}</h1>}
       {started ? <div className="flex-wrapper">
         <div className="findword-text">
           <p className="findword-text-p">
-            {textArray[sequence]}
+            {textArray[sequence].text}
           </p>
         </div>
         <div className="findword-answer">
-          <input className="findword-answer-input" placeholder="정답" type="number"></input>
-          <button className="findword-answer-next" onClick={submitAnswerHandler}>{`>`}</button>
+          <input className="findword-answer-input" placeholder="정답" type="number" onChange={onChangeHandler}></input>
+          <button className="findword-answer-next" disabled={!isEnterd} onClick={sequence === 3 ? finalSubmitHandler : submitAnswerHandler}>{`>`}</button>
         </div>
-      </div> : <button onClick={() => { setStarted(true) }}>시작</button>}
+      </div> : <button className="findword-start" onClick={() => { setStarted(true) }}>시작</button>}
     </div>
   </div>
 };
