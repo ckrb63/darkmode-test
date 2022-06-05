a17b71c2f81543e3602c63b8ae2b2258f67f768e
cksrb63 <cksr1@naver.com>
2022-06-05 17:24:27 +0900

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import Statics from './pages/Statics';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/find-word' element={<FindWord />} />
